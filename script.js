22b368ba613a0ade26f8e7fc0b3c05b03949db7d
ngroswn <39582304+michickomalanhoe@users.noreply.github.com>
2021-11-03 17:53:26 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function onClickMenu(){
 	document.getElementById("menu").classList.toggle("change");
+	document.getElementById("menu-bar").classList.toggle("change");
 	document.getElementById("nav").classList.toggle("change");
-	
 	document.getElementById("menu-bg").classList.toggle("change-bg");
 }
